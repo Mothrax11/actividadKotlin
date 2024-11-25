ef8cf44447f6baeb2d8b223be6c634abc92e06e7
Mothrax <raullg97@educastur.es>
2024-11-25 19:49:28 +0100

    Añadido metodo que compara parejas de llaves (no funciona de momento)

--- src/Ejercicio3.kt
+++ src/Ejercicio3.kt
@@ -2,7 +2,7 @@ import java.sql.SQLOutput
 
 fun main(){
 
-    var expresion:String = "{ [ a * ( c + d ) ] - 5 }";
+    var expresion:String = "{ ] a * ( c + d ) + ( 2 - 3 )[ - 5 }";
     var balanceada:Boolean = false;
     var delimitadores: MutableList<Char> = mutableListOf()
     var contador:Int = 0;
@@ -18,11 +18,36 @@ fun main(){
         }
         contador++;
     }
-
     println(delimitadores)
-    
+    var contadorResta:Int = delimitadores.size ;
+    var comparador:Int = delimitadores.size ;
+
     if(delimitadores.size % 2 != 0){
         println("Falta alguna llave de apertura o de cierre")
+    }
+    for (i in 0..delimitadores.size){
+        if((delimitadores.get(i) == '{') && (delimitadores.get(contadorResta) == '}')){
+            delimitadores.removeAt(i)
+            delimitadores.removeAt(contadorResta)
+            contadorResta--;
+            contador++;
+        } else if((delimitadores.get(i) == '[') && (delimitadores.get(contadorResta) == ']')){
+            delimitadores.removeAt(i)
+            delimitadores.removeAt(contadorResta)
+            contadorResta--;
+            contador++;
+        } else if((delimitadores.get(i) == '(') && (delimitadores.get(contadorResta) == ')')){
+            delimitadores.removeAt(i)
+            delimitadores.removeAt(contadorResta)
+            contadorResta--;
+            contador++;
+        }
+    }
+
+    if (contador == (comparador / 2)){
+        println("true")
+    } else{
+        println("false")
     }
 
 }
