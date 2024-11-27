288a506c7341b701aa799256a232f75a181dc133
Mothrax <raullg97@educastur.es>
2024-11-27 18:26:43 +0100

    Terminado el Ejercicio 5 en la feature Ejercicio5 Cambios a la hora de añadir "?" segun el tamaño de acciones o de la pista. Implementacion de lógica con un "when" para cada elmento de accion(mas fácil que comparar los 2 tipos de elementos (pista y accion))

--- src/Ejercicio4.kt
+++ src/Ejercicio4.kt
@@ -2,36 +2,57 @@ fun main(){
     var caso1Acciones: Array<String> = arrayOf("correr", "saltar", "correr", "saltar", "correr");
     var caso1Pista:String = "_|_|_"
     carrera(caso1Acciones, caso1Pista)
-
-
 }
 
 fun carrera(casoAccion:Array<String>, casoPista:String) {
 
-    val listaPista:MutableList<Char> = mutableListOf();
-    for (i in 0..casoPista.length - 1){
-        listaPista.add(casoPista.get(i))
-    }
-
-    var diferencia = 0;
-
-    /*crear when para cada accion (correr o saltar),
-    que compare con casoPista y realice lo oportuno */
-
-    if(casoPista.length > listaPista.size){
-        diferencia = casoPista.length - listaPista.size
-        /*creacion de pista auxiliar para complementar las diferencias de tamaño
-        (lo que antes eran huecos ahora serán null) para
-        facilitar el cambio en pista a las interrogaciones
-         */
-        for(i in 0..diferencia) {
-
+    /*val listaPista:MutableList<Char> = mutableListOf();
+        for (i in 0..casoPista.length - 1){
+            listaPista.add(casoPista.get(i))
         }
-    } else if (casoPista.length < listaPista.size){
-        diferencia = listaPista.size - casoPista.length
-    }
+    */
+
+    //Cambio de forma de hacer la lista (visto en los apuntes)(El comentario de arriba es mi antigua forma de hacerlo)
+    val listaPistaAux = casoPista.toMutableList()
+    val listaPista = casoPista.toMutableList()
+    var carreraSuperada = true
 
     for (k in casoAccion.indices){
-
+        if(k >= listaPistaAux.size){
+            listaPista.add('?')
+            carreraSuperada = false
+        }
     }
+
+    for (i in casoAccion.indices){
+        when(casoAccion[i]){
+            "correr" -> if(listaPista[i] == '_'){
+                            print("_")
+                        } else if(listaPista[i] == '|'){
+                            print("/")
+                            carreraSuperada = false
+                        } else {
+                            carreraSuperada = false
+                            print("?")
+                        }
+
+            "saltar" -> if(listaPista[i] == '|'){
+                            print("|")
+                        } else if(listaPista[i] == '_'){
+                            print("x")
+                            carreraSuperada = false
+                        } else {
+                            carreraSuperada = false
+                            print("?")
+                        }
+        }
+    }
+
+    if (casoAccion.size < listaPista.size) {
+        for (j in casoAccion.size until listaPista.size) {
+            print('?')
+            carreraSuperada = false
+        }
+    }
+    print(" $carreraSuperada")
 }
